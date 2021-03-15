0afcae6e527c362c42cbd50f79a68c68e338365a
ZPMan10197 <zeshawnmanzoor@yahoo.com>
2021-03-15 03:08:41 -0400

    project0 cars commit

--- csvreader/build.sbt
+++ csvreader/build.sbt
@@ -8,3 +8,4 @@ version := "1.0"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
 
+
--- csvreader/src/main/scala/CSVReader.scala
+++ csvreader/src/main/scala/CSVReader.scala
@@ -3,28 +3,19 @@ import java.io._
 
 object CSVReader extends App {
   
-  val filename = "covid19.csv"
+  val filename = "cars.csv"
   val bufferedSource = io.Source.fromFile(filename)
   val pw = new PrintWriter(new File("output.txt" ))
-  val pwExp = new PrintWriter(new File("exports.txt" ))
-  val pwImp = new PrintWriter(new File("imports.txt" ))
 
-  for(line <- bufferedSource.getLines().drop(1)){
-    val cols = line.split(",").map(_.trim)
-    var temp = cols(1).toInt
-    if(temp > 2019 && cols(4) == "United States"){
-      pw.write(s"${cols(0)}|${cols(1)}|${cols(4)}|${cols(5)}|${cols(9)} \n")
-    }
-    else if(cols(0) == "Exports"){
-      pwExp.write(s"${cols(0)}|${cols(1)}|${cols(4)}|${cols(5)}|${cols(9)} \n")
-    }
-    else{
-     pwImp.write(s"${cols(0)}|${cols(1)}|${cols(4)}|${cols(5)}|${cols(9)} \n")
-    }
+
+  for(line <- bufferedSource.getLines().drop(2)){
+    val cols = line.split(";").map(_.trim)
+    pw.write(s"${cols(0)} | ${cols(1)} | ${cols(4)} | ${cols(6)} | ${cols(8)} \n")
+    
+    
+   
     
   }
-  pwExp.close()
-  pwImp.close()
   pw.close()
   bufferedSource.close()
   
